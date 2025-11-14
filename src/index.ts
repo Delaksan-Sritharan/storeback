8d7c04f20a5adf4f9aeaaf290b537aaf13ece872
Delaksan-Sritharan <delaksan@gmail.com>
2025-11-14 10:52:20 +0530

    feat: implement global error handling middleware
    refactor: migrate review API from JavaScript to TypeScript
    
    refactor: migrate hotel API from JavaScript to TypeScript
    
    refactor: migrate location API from JavaScript to TypeScript
    
    refactor: migrate application logic for hotel and location to TypeScript
    
    refactor: migrate application logic for review to TypeScript
    
    refactor: migrate error handling classes to TypeScript
    
    chore: add global TypeScript definitions
    
    chore: set up TypeScript configuration
    
    feat: implement Booking entity and its schema
    
    refactor: migrate database connection logic to TypeScript
    
    chore: update seed script to reflect new structure and entities
    
    refactor: migrate User entity to TypeScript
    
    refactor: migrate infrastructure entities to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
-import hotelsRouter from "./api/hotel.js";
-import connectDB from "./infrastructure/db.js";
-import reviewRouter from "./api/review.js";
-import locationsRouter from "./api/location.js";
-import globalErrorHandlingMiddleware from "./api/middleware/global-error-handling-middleware.js";
+import hotelsRouter from "./api/hotel";
+import connectDB from "./infrastructure/db";
+import reviewRouter from "./api/review";
+import locationsRouter from "./api/location";
+import globalErrorHandlingMiddleware from "./api/middleware/global-error-handling-middleware";
 
 import { clerkMiddleware } from "@clerk/express";
 
@@ -16,9 +16,9 @@ const app = express();
 // Convert HTTP payloads into JS objects
 app.use(express.json());
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
+  cors({
+    origin: "http://localhost:5173",
+  })
 );
 app.use(clerkMiddleware()); // Reads the JWT from the request and sets the auth object on the request
 
@@ -37,5 +37,5 @@ connectDB();
 
 const PORT = 8000;
 app.listen(PORT, () => {
-    console.log("Server is listening on PORT: ", PORT);
+  console.log("Server is listening on PORT: ", PORT);
 });
